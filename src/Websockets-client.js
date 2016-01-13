2b72d856cf270bafbb13a5a66c44c956c98533b8
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2016-01-13 11:17:44 +0100

    undefine define (for requirejs compatibility)

--- src/Websockets-client.js
+++ src/Websockets-client.js
@@ -1,7 +1,15 @@
-/* global Y */
+/* global Y, global */
 'use strict'
 
+// socket.io requires utf8. This package checks if it is required by requirejs.
+// If window.require is set, then it will define itself as a module. This is erratic behavior and 
+// results in socket.io having a "bad request".
+// This is why we undefine global.define (it is set by requirejs) before we require socket.io-client.
+var define = global.define
+global.define = null
 var io = require('socket.io-client')
+// redefine global.define
+global.define = define
 
 function extend (Y) {
   class Connector extends Y.AbstractConnector {
